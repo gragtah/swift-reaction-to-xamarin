b831b1cd9b7b2e478c893651fc22595e6ee2e7ce
Pratyush Nalam <pratyushnalam@outlook.com>
2016-04-23 21:19:45 -0400

    Sample location app

---
+++ xamarin/Accessibility/Accessibility/Application.cs
@@ -0,0 +1,20 @@
+using System;
+using UIKit;
+
+namespace Accessibility
+{
+	public class Application// : UIApplication
+	{
+		public static void Main (string[] args)
+		{
+			try
+			{
+				UIApplication.Main (args, null, "AppDelegate");
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine (e.ToString ());
+			}
+		}
+	}
+}
---
+++ xamarin/Accessibility/Accessibility/MainScreen/IMainScreen.cs
@@ -0,0 +1,20 @@
+using System;
+using UIKit;
+
+namespace Accessibility
+{
+	/// <summary>
+	/// This interface is so we can use a different xib for iPhone and iPad from the same code.
+	/// </summary>
+	public interface IMainScreen
+	{
+		UILabel LblAltitude { get; }
+		UILabel LblLatitude { get; }
+		UILabel LblLongitude { get; }
+		UILabel LblCourse { get; }
+		UILabel LblMagneticHeading { get; }
+		UILabel LblSpeed { get; }
+		UILabel LblTrueHeading { get; }
+		UILabel LblDistanceToParis { get; }
+	}
+}
